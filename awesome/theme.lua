6d78cb518670c4a9f8a8cc2eb6c5a38541531788
asevcsik <andras.sevcsik@performgroup.com>
2014-10-20 16:01:59 +0200

    Awesome config updated

--- awesome/theme.lua
+++ awesome/theme.lua
@@ -73,7 +73,7 @@ theme.titlebar_maximized_button_focus_inactive  = "/usr/share/awesome/themes/def
 theme.titlebar_maximized_button_normal_active = "/usr/share/awesome/themes/default/titlebar/maximized_normal_active.png"
 theme.titlebar_maximized_button_focus_active  = "/usr/share/awesome/themes/default/titlebar/maximized_focus_active.png"
 
-theme.wallpaper = "~/.config/awesome/backgrounds/daskeyboard-4-ultimate-rear-view-carousel.jpg"
+theme.wallpaper = "~/.config/awesome/backgrounds/http___thehdwall.com_wp-content_uploads_2014_09_Railroad-Rails-Colorsplash-Widescreen.jpg"
 
 -- You can use your own layout icons like this:
 theme.layout_fairh = "/usr/share/awesome/themes/default/layouts/fairhw.png"
