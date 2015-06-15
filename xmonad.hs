bbb4e11807401dbe19b41d2d38ae9e7b56daa59b
Andras Sevcsik <Andras.Sevcsik@performgroup.com>
2015-06-15 14:34:30 +0200

    xmobar urgency handling

--- xmonad.hs
+++ xmonad.hs
@@ -1,22 +1,42 @@
 import XMonad
 import XMonad.Hooks.DynamicLog
 import XMonad.Layout.NoBorders
+import XMonad.Hooks.UrgencyHook
+import XMonad.Hooks.ManageDocks
+import XMonad.Util.Run
+import System.IO
 
-general = defaultConfig { modMask = mod4Mask
-                        , terminal = "gnome-terminal"
-                        }
+main = do
+    pipe <- spawnPipe "xmobar"
+    let config' = defaultConfig { modMask = mod4Mask
+                                , terminal = "gnome-terminal"
+                                , focusedBorderColor="#888888"
+                                , normalBorderColor = "#000000"
+                                , layoutHook = layout'
+                                , workspaces = workspaces'
+                                , logHook = log' pipe
+                                , manageHook = manage'
+                                }
 
-borders = general { focusedBorderColor="#888888"
-                  , normalBorderColor = "#000000"
-                  , layoutHook = smartBorders $ layoutHook defaultConfig
-                  }
+    xmonad $ withUrgencyHook NoUrgencyHook $ config'
 
-workspaces' = borders { workspaces = [ "test" 
-                                     , "dev"
-                                     , "media"
-                                     , "msg"
-                                     ] ++ map show [5..9]
-                      }
+log' pipe = dynamicLogWithPP xmobarPP
+    { ppCurrent = xmobarColor "yellow" ""
+    , ppUrgent = xmobarColor "red" ""
+    , ppOutput = hPutStrLn pipe
+    }
 
-main = xmonad =<< xmobar workspaces'
+layout' = ( avoidStruts
+          . smartBorders
+          . layoutHook
+          ) defaultConfig
+
+manage' = manageHook defaultConfig <+> manageDocks 
+
+workspaces' = [ "test" 
+              , "dev"
+              , "media"
+              , "msg"
+              , "msg2"
+              ] ++ map show [5..9]
 
