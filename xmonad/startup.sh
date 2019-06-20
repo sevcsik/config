1829934f287c2a85bf40b61c1740ecae54a1c49b
András Sevcsik-Zajácz <sevcsik@sevdev.hu>
2019-06-20 07:56:42 +0200

    updated old xmonad.hs with new one

--- xmonad/startup.sh
+++ xmonad/startup.sh
@@ -4,3 +4,4 @@ xcompmgr &
 trayer --edge top --align right --expand true --widthtype request --transparent true --tint 0 &
 nm-applet &
 cbatticon &
+pnmixer &
--- xmonad/xmonad.hs
+++ xmonad/xmonad.hs
@@ -1,8 +1,52 @@
 import XMonad
+import XMonad.Config.Kde
+import XMonad.Hooks.DynamicLog
+import XMonad.Layout.NoBorders
+import XMonad.Hooks.UrgencyHook
+import XMonad.Hooks.ManageDocks
+import XMonad.Hooks.EwmhDesktops
+import XMonad.Hooks.ManageHelpers ( doFullFloat, isFullscreen )
+import XMonad.Util.Run
 import XMonad.Util.SpawnOnce
+import System.IO
 
-cfg = defaultConfig { modMask = mod4Mask 
-                    , startupHook = spawnOnce "./.xmonad/startup.sh"
-		    }
+customRules = composeAll [ className =? "plasmashell" --> doIgnore ]
 
-main = xmonad $ cfg
+main = do
+    let config' = kde4Config { modMask = mod4Mask
+                             , focusedBorderColor="#ff00ff"
+                             , handleEventHook = handleEvent'
+                             , layoutHook = layout'
+                             , manageHook = manage'
+                             , normalBorderColor = "#000000"
+                             , startupHook = spawnOnce "./.xmonad/startup.sh"
+                             , terminal = "konsole"
+                             , workspaces = workspaces'
+                             }
+
+    xmonad $ ewmh 
+           $ (withUrgencyHook NoUrgencyHook) 
+           $ config'
+
+layout' = ( avoidStruts
+          . smartBorders
+          ) (layoutHook defaultConfig)
+
+manage' = ( isFullscreen --> doFullFloat)
+      <+> manageHook defaultConfig
+      <+> customRules
+      <+> manageDocks 
+
+handleEvent' = handleEventHook defaultConfig
+           <+> docksEventHook
+           <+> fullscreenEventHook
+
+workspaces' = [ "test" 
+              , "dev"
+              , "msg"
+              , "msg2"
+              ] ++ map show [5..7] ++
+              [ "media"
+              , "mixer"
+              ]
+
