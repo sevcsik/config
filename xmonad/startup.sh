08a5280c0c32f1cbcda5f720f1a1602e29fb8ead
Sevcsik-Zajácz András <sevcsik-zajacz.andras@gbsolutions.io>
2019-09-03 16:41:24 +0200

    Background, email signatures

--- xmonad/startup.sh
+++ xmonad/startup.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 $HOME/.screenlayout/default.sh
+$HOME/.screenlayout/background.sh
 xcompmgr &
 trayer --edge top --align right --expand true --widthtype request --transparent true --tint 0 &
 nm-applet &
