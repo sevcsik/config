155774935e23f23a395920d8010c1d33975db52d
Sevcsik-Zajácz András <sevcsik-zajacz.andras@gbsolutions.io>
2020-09-06 22:52:26 +0200

    Added clojure emacs packages, emacs config

--- emacs.el
+++ emacs.el
@@ -5,10 +5,12 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(custom-enabled-themes '(solarized-light-high-contrast))
+ '(custom-enabled-themes (quote (solarized-light-high-contrast)))
  '(custom-safe-themes
-   '("00445e6f15d31e9afaa23ed0d765850e9cd5e929be5e8e63b114a3346236c44c" default))
- '(org-agenda-files '("~/Dokumentumok/Jegyzetek")))
+   (quote
+    ("00445e6f15d31e9afaa23ed0d765850e9cd5e929be5e8e63b114a3346236c44c" default)))
+ '(inhibit-startup-screen t)
+ '(org-agenda-files (quote ("~/Dokumentumok/Jegyzetek"))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
--- nixpkgs/emacs.nix
+++ nixpkgs/emacs.nix
@@ -4,7 +4,12 @@
   ...
 }:
 (emacsPackagesGen emacs).emacsWithPackages (epkgs:[
+  epkgs.melpaPackages.cider
+  epkgs.melpaPackages.clojure-mode
+  epkgs.melpaPackages.direnv
+  epkgs.melpaPackages.emmet-mode
   epkgs.melpaPackages.evil
+  epkgs.melpaPackages.nix-mode
   epkgs.melpaPackages.solarized-theme
 ])
 
