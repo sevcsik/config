af04dc81b1231a9b21980c61d4eaa521f84c9575
Sevcsik-Zajácz András <sevcsik-zajacz.andras@gbsolutions.io>
2021-05-25 19:00:16 +0200

    projectile config, use-packge

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -33,21 +33,57 @@
   (load (concat (file-name-directory load-file-name) "early-init")
         nil t))
 
-;; And let 'er rip!
+;; Ensure Doom's core libraries are properly initialized, autoloads file is
+;; loaded, and hooks set up for an interactive session.
 (doom-initialize)
 
-(editorconfig-mode 1)
-(treemacs-resize-icons 11)
+;; Now we load all enabled modules in the order dictated by your `doom!' block
+;; in $DOOMDIR/init.el. `doom-initialize-modules' loads them (and hooks) in the
+;; given order:
+;;
+;;   $DOOMDIR/init.el
+;;   {$DOOMDIR,~/.emacs.d}/modules/*/*/init.el
+;;   `doom-before-init-modules-hook'
+;;   {$DOOMDIR,~/.emacs.d}/modules/*/*/config.el
+;;   `doom-init-modules-hook'
+;;   $DOOMDIR/config.el
+;;   `doom-after-init-modules-hook'
+;;   `after-init-hook'
+;;   `emacs-startup-hook'
+;;   `doom-init-ui-hook'
+;;   `window-setup-hook'
+;;
+;; And then we're good to go!
+(doom-initialize-modules)
+
 (setq auth-sources '("~/.authinfo"))
-(make-directory "~/.org-jira" t)
-(setq jiralib-url "https://gb-solutions.atlassian.net")
 
 (add-hook 'eshell-mode-hook (lambda() (setenv "GIT_PAGER" "")
                                       (setenv "PAGER" "cat")
                                       (setenv "TERM" "dumb")))
 
-;; (use-package typescript-mode
-;;   :hook (typescript-mode . lsp))
+(use-package editorconfig :init (editorconfig-mode 1))
 
-;; (use-package web-mode
-;;   :hook (web-mode . (lambda() (when (string-match-p "\.component\.html\'" (buffer-file-name)) (lsp "angular")))))
+(use-package treemacs :init (treemacs-resize-icons 11))
+
+(use-package org-jira
+  :init
+  (make-directory "~/.org-jira" t)
+  (setq jiralib-url "https://gb-solutions.atlassian.net"))
+
+(use-package projectile
+  :config
+  (setq projectile-completion-system 'ivy)
+  (setq projectile-enable-caching t)
+  (setq projectile-globally-ignored-directories
+        '("node_modules" "dist" "target" ".mvn"))
+  (setq projectile-indexing-method 'native)
+  (setq projectile-project-search-path '("~/Hobbi"))
+  (setq projectile-switch-project-action #'projectile-commander)
+  (projectile-register-project-type 'npm '("package.json")
+    :project-file "package.json"
+    :compile "npm run build"
+    :test "npm test"
+    :run "npm start"
+    :test-suffix ".spec"))
+  (projectile-mode t)
